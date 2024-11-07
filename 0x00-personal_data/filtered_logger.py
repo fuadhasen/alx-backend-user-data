ad3c829ca8958f58052daef35dec98692ed223e3
fuadhasen <fuadhas6634@gmail.com>
2024-11-07 12:07:17 +0530

    filtered_logger updated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """script for filtered logger"""
 import re
+import logging
 from typing import List
 
 
@@ -12,3 +13,24 @@ def filter_datum(fields: List[str],
         message = re.sub(rf"{field}=[^{separator}]*",
                          f"{field}={redaction}", message)
     return separator.join(message.split(separator))
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        filter_mssge = filter_datum(self.fields,
+                                    self.REDACTION,
+                                    record.getMessage(), self.SEPARATOR)
+        original_mssge = super().format(record)
+        return original_mssge.replace(record.getMessage(),
+                                      filter_mssge)
