07239eba3d00173ec6d4a91dec292152451015f7
fuadhasen <fuadhas6634@gmail.com>
2024-11-07 16:11:37 +0530

    final update encrypt pwd

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,9 @@ def hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(password.encode('UTF-8'),
                            bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """verification of password"""
+    return bcrypt.checkpw(password.encode('UTF-8'),
+                          hashed_password)
