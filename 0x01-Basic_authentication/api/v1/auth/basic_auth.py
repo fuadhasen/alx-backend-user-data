01a8cc1d902c1b8d31adf1b5a2b5822e1b491a77
fuadhasen <fuadhas6634@gmail.com>
2024-11-12 00:11:11 +0530

    add task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,16 @@ from typing import List, TypeVar
 class BasicAuth(Auth):
     """Basic Authentication Simulation
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """base64 decoding"""
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        value = authorization_header.split(' ')[1]
+        return value
