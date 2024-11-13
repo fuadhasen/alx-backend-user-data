fcd2df2e6bb19e00b4edb72dbc80de31182804c6
fuadhasen <fuadhas6634@gmail.com>
2024-11-13 23:52:24 +0530

    update task 7

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -6,6 +6,7 @@ from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
 
+
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def auth_session_login() -> str:
     """ GET /api/v1/auth_session/login
@@ -14,18 +15,18 @@ def auth_session_login() -> str:
     """
     email = request.form.get('email')
     if not email:
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
 
     password = request.form.get('password')
     if not password:
-        return jsonify({ "error": "password missing" }), 400
+        return jsonify({"error": "password missing"}), 400
 
     users = User.search({"email": email})
     if not users:
-        return jsonify({ "error": "no user found for this email" }), 404
-    
+        return jsonify({"error": "no user found for this email"}), 404
+
     if users[0].is_valid_password(password) is not True:
-        return jsonify({ "error": "wrong password" }), 401
+        return jsonify({"error": "wrong password"}), 401
     from api.v1.app import auth
     session_id = auth.create_session(users[0].id)
     res = jsonify(users[0].to_json())
