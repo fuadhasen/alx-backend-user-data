f4f759d513861f22d7cc6ef2c46cdd8449ab0947
fuadhasen <fuadhas6634@gmail.com>
2024-11-13 14:22:03 +0530

    update

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,9 +7,22 @@ from .auth import Auth
 from typing import List, TypeVar, Tuple
 from models.base import Base
 from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
     """Session Authentication manual Implementation
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """instance method to create session
+        """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+
+        session_id = uuid.uuid4()
+        self.user_id_by_session_id[str(session_id)] = user_id
+        return str(session_id)
