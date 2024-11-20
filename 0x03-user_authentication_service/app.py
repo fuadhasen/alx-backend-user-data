7bce94a06aa3535e54c5a4ffc7ebb7824cf1df5d
fuadhasen <fuadhas6634@gmail.com>
2024-11-20 12:36:42 +0530

    update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -90,14 +90,14 @@ def reset():
     """method to register users
     """
     email = request.form .get("email")
-    reset_token = request.form.get("reset_token")
-    new_password = request.form.get("new_password")
+    token = request.form.get("reset_token")
+    pwd = request.form.get("new_password")
 
     try:
-        AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"})
+        AUTH.update_password(reset_token=token, password=pwd)
     except ValueError:
         abort(403)
+    return jsonify({"email": email, "message": "Password updated"})
 
 
 if __name__ == "__main__":
